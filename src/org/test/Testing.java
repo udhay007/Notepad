e5c86bfc24e38ec43835d21d4ceda23941b16d78
udhay007 <udhaysmart8@gmail.com>
2022-01-04 10:06:20 +0530

    File structure

---
+++ src/org/test/Testing.java
@@ -0,0 +1,40 @@
+package org.test;
+
+import java.lang.invoke.SwitchPoint;
+
+public class Testing {
+	public static void main(String[] args) {
+		
+		String d="sunday";
+		switch (d) {
+		case "sunday":
+			System.out.println("holiday");
+			
+			break;
+		case "tuesday":
+			System.out.println("full holiday");
+			
+			break;
+		case "wednesday":
+			System.out.println("half holiday");
+			
+			break;
+		case "thursday":
+			System.out.println("full  workingday");
+			
+			break;
+
+		default:
+			System.out.println("default method");
+			break;
+			
+		}
+		
+		
+		
+		
+	}
+		
+		
+	}
+	
